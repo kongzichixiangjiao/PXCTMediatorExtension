5e0c8913630cba0544ed08b49a04ae238e88b26d
kongzichixiangjiao <58895347@qq.com>
2019-01-17 10:11:51 +0800

    0.0.1

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PXCTMediatorExtension
+//
+//  Created by 侯佳男 on 2019/1/17.
+//  Copyright © 2019年 侯佳男. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
